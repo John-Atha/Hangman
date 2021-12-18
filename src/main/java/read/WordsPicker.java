140439e0ff85c908d808db6775a3a7282fcf714d
John-Atha <giannisj3@gmail.com>
2021-12-18 21:26:23 +0200

    Add dummy description words filtering

--- src/main/java/read/WordsPicker.java
+++ src/main/java/read/WordsPicker.java
@@ -1,17 +1,45 @@
 package read;
 
-import java.io.IOException;
+import java.util.ArrayList;
 import org.json.*;
 
 public class WordsPicker {
 
-    public static void main(String[] args) {
-        String requestUrl = "https://openlibrary.org/works/OL45883W.json";
+    private String description;
+    private ArrayList<String> words;
+
+    public String getDescription() {
+        return this.description;
+    }
+    private void setDescription(String description) {
+        this.description = description;
+        this.words = this.filterDescription(description);
+    }
+    public ArrayList<String> getWords() {
+        return this.words;
+    }
+
+    private ArrayList<String> filterDescription(String description) {
+        String[] parts = description.split(" ");
+        ArrayList<String> parts_ = new ArrayList<String>();
+        for (String part : parts) {
+            parts_.add(part);
+        }
+        return parts_;
+    }
+    
+    public WordsPicker(String requestUrl) {
         DataFetcher fetcher = new DataFetcher(requestUrl);
         fetcher.fetchData();
         JSONObject data = fetcher.getRequestData();
         String description = data.getString("description");
-        System.out.println(description);
+        setDescription(description);
+    }
+
+    public static void main(String[] args) {
+        String requestUrl = "https://openlibrary.org/works/OL45883W.json";
+        WordsPicker wordsPicker = new WordsPicker(requestUrl);
+        System.out.println(wordsPicker.getWords().toString());
     }
     
 }
